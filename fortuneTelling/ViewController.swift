e28b0cc0e97e897a69f557e9ebccda3a79daf390
Shien <f0928652413@gmail.com>
2022-03-04 13:34:51 +0800

    Initial Commit

---
+++ fortuneTelling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fortuneTelling
+//
+//  Created by 方仕賢 on 2022/3/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
